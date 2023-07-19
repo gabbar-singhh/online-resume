c963258e778ce0fc1e1eb9c36f0aeb353496b8e0
himanshu <himanshu01.dev@gmail.com>
2023-07-19 18:30:21 +0530

    temp-page :added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Online Resumes - Create Your Resume in Minutes</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.webp" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
